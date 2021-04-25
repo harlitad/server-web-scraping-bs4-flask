a703c6b2c5a60429079b3d1dd1a194637da17ce4
harlita <harlitad@gmail.com>
2021-04-25 11:49:45 +0700

    create flask app

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import request, Response
+from flask import url_for, redirect, templating
+# from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+@app.route("/api/scrap")
+def route():
+    result_scraping = scrap()
+    if request.method == "GET":
+        return {
+            "message": "Welcome Peeps!",
+            "data": result_scraping
+        }, 200
+
+
+@app.errorhandler(404)
+def error_not_found(e):
+    return {
+        "message": "sorry, we can't find the route."
+    }, 404
+
+
+if __name__ == "__main__":
+    app.run()
