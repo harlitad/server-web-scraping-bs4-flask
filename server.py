ae0629e088da5f4dd468cea9ccc3933fe546c58f
harlita <harlitad@gmail.com>
2021-04-25 12:14:56 +0700

    fixing server app

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import request, Response
 from flask import url_for, redirect, templating
-# from flask_cors import CORS
+from flask_cors import CORS
+from scraping import scrap
 
 app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
