d060a7cdc12d82eb65edecd3a12fea3cdbcd4ae4
harlita <harlitad@gmail.com>
2021-04-25 16:13:06 +0700

    fixing scrap func

--- scraping.py
+++ scraping.py
@@ -6,15 +6,18 @@ def scrap():
 
     getData = requests.get(
         'http://mlg.ucd.ie/modules/COMP41680/assignment2/month-jan-001.html').text
-    soup = BeautifulSoup(getData, "html.parser")
+    soup = BeautifulSoup(getData)
     # get all content inside class article, it will wrapped in an array object
     all_article = soup.find_all("div", {"class": "article"})
 
     # loop to get all the tag anchor and get the href attribute
-    # for article in all_article:
-    #     link = article.find('a')
-    #     url = link.get('href')
-    #     print(url)
-
-    # loop using list generator
-    return [article.find('a').get('href') for article in all_article]
+    data = []
+    for article in all_article:
+        link = article.find('a')
+        url = link.get('href')
+        title = link.string.encode("utf-8").decode("utf-8")
+        data.append({
+            "title" : title,
+            "link" : url,   
+        })
+    return data
